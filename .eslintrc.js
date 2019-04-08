98bd4b8d9fe4b656ff0f20329f0092a217dd1cd8
Benjamin Kroeger <benjamin.kroeger@gmail.com>
2019-04-08 23:58:20 +0200

    updates dependencies and formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,25 @@
-module.exports = { extends: 'oniyi' };
+'use strict';
+
+// node core
+
+// third-party
+
+// internal
+
+module.exports = {
+  parserOptions: {
+    sourceType: 'script',
+    ecmaFeatures: {
+      jsx: false,
+    },
+  },
+  env: {
+    jest: true,
+    node: true,
+  },
+  plugins: ['prettier'],
+  extends: ['airbnb-base', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// node core
+
+// third-party
+
+// internal
+
+module.exports = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+};
